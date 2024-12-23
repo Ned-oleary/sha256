1609e7902fffb29da106b44c5169a68d54c7f83c
Ned O'Leary <edwardkoleary@gmail.com>
2024-12-23 11:17:31 -0800

    refactor

--- eko_sha256.py
+++ eko_sha256.py
@@ -1,11 +1,15 @@
 
 # constants defined as part of SHA256
+# these are just 'nothing up my sleeve constants' 
+# they make clear that there's no backdoor
 H = [
     0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
     0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
 ]
 
 # constants defined as part of SHA256
+# these are just 'nothing up my sleeve' constants
+# they make clear that there's no backdoor
 K = [
     0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
     0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
@@ -21,29 +25,32 @@ def length_in_bits(input: bytes) -> int:
     return len(input) * 8
 
 
-def is_448_mod_512(length: int) -> bool:
+def is_64_bits_away_from_multiple_of_512(length: int) -> bool:
     return length % 512 == 448
+
+def append_1_bit(input: bytes) -> bytes:
+    return input + b'\x80'
+
+def append_0_bit(input: bytes) -> bytes:
+    return input + b'\x00'
+
+def append_64_bit_integer(input: bytes, integer: int) -> bytes:
+    return input + integer.to_bytes(8, 'big')
 
 
 def pad_input(input: bytes) -> bytes:
-
     input_bit_length = length_in_bits(input)
-    padded_input = input + b'\x80' # append 1 bit
-
-    # append K '0' bits, where K is the minimum number >= 0 such that (L + 1 + K + 64) is a multiple of 512"
-    while not is_448_mod_512(length_in_bits(padded_input)):
-        padded_input += b'\x00'
-
-    # append L as a 64-bit big-endian integer, making the total post-processed length a multiple of 512 bits
-    padded_input += input_bit_length.to_bytes(8, 'big')
-
+    padded_input = append_1_bit(input) 
+    while not is_64_bits_away_from_multiple_of_512(length_in_bits(padded_input)):
+        padded_input = append_0_bit(padded_input)
+    padded_input = append_64_bit_integer(padded_input, input_bit_length)
     return padded_input
 
 
-def message_partition(padded_input: bytes) -> list[bytes]:
+def input_partition(input: bytes) -> list[bytes]:
     chunk_array = []
-    for i in range(0, len(padded_input), 64):
-        chunk = padded_input[i:i+64]
+    for i in range(0, len(input), 64):
+        chunk = input[i:i+64]
         chunk_array.append(chunk)
     return chunk_array
 
@@ -61,25 +68,24 @@ def rotr(value: bytes, shifts: int) -> bytes:
 
 
 def sha256_compress(chunk: bytes, H: list[bytes]) -> bytes:
-    # create a 64-entry message schedule array w[0..63] of 32-bit words
-    w_array = []
-    for i in range(0, 64, 4):
+    word_array = []
+    for i in range(0, 64, 4): # every 32 bits
         four_bytes = chunk[i:i+4]
         word = int.from_bytes(four_bytes, 'big')
-        w_array.append(word)
+        word_array.append(word)
 
     for i in range(16, 64):
-        s0 = rotr(w_array[i-15], 7) ^ rotr(w_array[i-15], 18) ^ right_shift(w_array[i-15], 3)
-        s1 = rotr(w_array[i-2], 17) ^ rotr(w_array[i-2], 19) ^ right_shift(w_array[i-2], 10)
-        new_word = (w_array[i-16] + s0 + w_array[i-7] + s1) & 0xFFFFFFFF
-        w_array.append(new_word)
+        s0 = rotr(word_array[i-15], 7) ^ rotr(word_array[i-15], 18) ^ right_shift(word_array[i-15], 3)
+        s1 = rotr(word_array[i-2], 17) ^ rotr(word_array[i-2], 19) ^ right_shift(word_array[i-2], 10)
+        new_word = (word_array[i-16] + s0 + word_array[i-7] + s1) & 0xFFFFFFFF
+        word_array.append(new_word)
 
     a,b,c,d,e,f,g,h = H
 
     for i in range(64):
         S1 = rotr(e, 6) ^ rotr(e, 11) ^ rotr(e, 25)
         ch = (e & f) ^ ((~ e) & g)
-        temp1 = h + S1 + ch + K[i] + w_array[i]
+        temp1 = h + S1 + ch + K[i] + word_array[i]
         S0 = rotr(a, 2) ^ rotr(a, 13) ^ rotr(a, 22)
         maj = (a & b) ^ (a & c) ^ (b & c)
         temp2 = (S0 + maj) & 0xFFFFFFFF
@@ -108,6 +114,6 @@ def sha256_compress(chunk: bytes, H: list[bytes]) -> bytes:
 def sha256(input: bytes, H: list[bytes] = H) -> bytes:
     assert isinstance(input, bytes)
     padded_input = pad_input(input)
-    for chunks in message_partition(padded_input):
+    for chunks in input_partition(padded_input):
           H = sha256_compress(chunks, H)
     return ''.join(f'{x:08x}' for x in H)
